782eacf6c9d891fe07cc204a7dc128c5f1aa7e6c
xausee <xausee@gmail.com>
2016-01-24 15:39:15 +0800

    firt commit

---
+++ app/src/main/java/com/example/administrator/examsystem/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.administrator.examsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webView_Main);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webview.loadUrl("http://42.200.39.108/admin");
+    }
+}
